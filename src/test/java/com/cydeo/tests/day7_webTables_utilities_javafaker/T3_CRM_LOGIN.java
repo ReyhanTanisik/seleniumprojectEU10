93f38e4b4ee84ea2742b0d7c0f4946f4b378f4e4
Reyhan <rcorek@gmail.com>
2023-01-10 21:04:58 +0300

    task 3 done

---
+++ src/test/java/com/cydeo/tests/day7_webTables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day7_webTables_utilities_javafaker;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T3_CRM_LOGIN {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+    }
+
+    @Test
+    public void crm_login_test(){
+        //1. Create new test and make setups
+        //2. Go to : http://login1.nextbasecrm.com/
+
+        driver.get("http://login1.nextbasecrm.com/");
+
+        //3. Enter valid username
+
+        WebElement usernameBox= driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        usernameBox.sendKeys("helpdesk1@cybertekschool.com");
+
+        //4. Enter valid password
+
+        WebElement passwordBox=driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        passwordBox.sendKeys("UserUser");
+        //5. Click to `Log In` button
+
+        WebElement loginButton= driver.findElement(By.xpath("//input[@type='submit']"));
+        loginButton.click();
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver,"My tasks");
+    }
+
+}
