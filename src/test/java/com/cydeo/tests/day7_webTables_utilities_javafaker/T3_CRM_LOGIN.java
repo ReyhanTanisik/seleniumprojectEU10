5bf0021529e0df8d2ea7882c2eb51c8e031d48b7
Reyhan <rcorek@gmail.com>
2023-01-10 21:44:06 +0300

    crm login method used in T3

--- src/test/java/com/cydeo/tests/day7_webTables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webTables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day7_webTables_utilities_javafaker;
 
 import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -30,7 +31,6 @@ public class T3_CRM_LOGIN {
         driver.get("http://login1.nextbasecrm.com/");
 
         //3. Enter valid username
-
         WebElement usernameBox= driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
         usernameBox.sendKeys("helpdesk1@cybertekschool.com");
 
@@ -42,9 +42,26 @@ public class T3_CRM_LOGIN {
 
         WebElement loginButton= driver.findElement(By.xpath("//input[@type='submit']"));
         loginButton.click();
+
         //6. Verify title is as expected:
         //Expected: Portal
         BrowserUtils.verifyTitle(driver,"My tasks");
     }
 
+
+    @Test
+    public void crm_login_test2(){
+        //1. Create new test and make setups
+        //2. Go to : http://login1.nextbasecrm.com/
+
+        driver.get("http://login1.nextbasecrm.com/");
+
+        //calling my utility method to login helpdesk
+
+
+        CRM_Utilities.crm_login(driver);
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver,"My tasks");
+    }
 }
