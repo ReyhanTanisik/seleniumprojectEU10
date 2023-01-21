966c72a3382cbe11910280be3ab7345449edb29f
Reyhan <rcorek@gmail.com>
2023-01-21 21:14:25 +0300

    configuration properties practice task

---
+++ src/test/java/com/cydeo/tests/day8_properties_configReader/T4_Config_Practice.java
@@ -0,0 +1,46 @@
+package com.cydeo.tests.day8_properties_configReader;
+
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_Config_Practice {
+
+    //1- Open a chrome browser
+    //2- Go to: https://google.com
+    //3- Write “apple” in search box
+    //4- Verify title:
+    //Expected: apple - Google Search
+    //
+    WebDriver driver;
+    @BeforeMethod
+    public void setUpMethod(){
+
+        // we are getting the browser type dynamically from our configuration.properties file
+        String browserType= ConfigurationReader.getProperty("browser");
+
+        driver= WebDriverFactory.getDriver(browserType);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+    }
+
+    @Test
+    public void google_Search_Test(){
+        WebElement googleSearchBox= driver.findElement(By.xpath("//input[@name='q']"));
+        googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue")+ Keys.ENTER);
+
+        String expectedTitle=ConfigurationReader.getProperty("searchValue")+" - Google'da Ara";
+        String actualTitle=driver.getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+    }
+}
